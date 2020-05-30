0f69d012ab3bcd0e436d4a7a8ba552ea7f48f17a
Eric Zhang <ezhang887@gmail.com>
2020-05-29 23:06:40 -0700

    Add support for multiple video files + small cleanup

--- scripts/VLCLabeller.py
+++ scripts/VLCLabeller.py
@@ -1,5 +1,8 @@
 import vlc
+import os
 import signal
+import glob
+import argparse
 import sys
 import time
 import threading
@@ -8,40 +11,46 @@ from pynput import keyboard
 
 class Labeller():
 
-    def __init__(self, video):
-
-        self.video = video
-
-        self.player = vlc.MediaPlayer(video)
-        self.player.play()
-
-        listener = keyboard.Listener(
-                on_press=self._on_press)
-        listener.start()
+    def __init__(self):
+        # setup keyboard listener
+        self.listener = keyboard.Listener(on_press=self._on_press)
+        self.listener.start()
 
         self.intro_start = None
         self.intro_end = None
         self.outro_start = None
         self.outro_end = None
  
-        time.sleep(0.1) # hack
-        self.end_time = self.player.get_media().get_duration()
-        
         self.skip_time = 5000 # 5 sec
-        self.end_margin = 500 #0.5 sec
-        self.thread_period = 0.1 #0.1 sec
+        self.end_margin = 500 # 0.5 sec
+        self.thread_period = 0.1 # 0.1 sec
         self.numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-        self.running = True
+        # setup watchdog and signal handler
         signal.signal(signal.SIGINT, self._sigint_handler)
 
-        t1 = threading.Thread(target=self._watchdog)
-        t1.start()
-        t1.join()
+    def run(self, video_path):
+        # start playing video
+        self.player = vlc.MediaPlayer(video_path)
+        self.player.play()
+        self.running = True
+        time.sleep(0.1) # hack
+        self.end_time = self.player.get_media().get_duration()
+
+        # start watchdog
+        watchdog = threading.Thread(target=self._watchdog)
+        watchdog.start()
+
+        # wait until the watchdog has exited
+        watchdog.join()
+
+        # and cleanup
         self.player.stop()
 
     def _sigint_handler(self, signum, frame):
         self.running = False
+        self.listener.stop()
+        sys.exit(0)
 
     def _watchdog(self):
         while self.running:
@@ -57,27 +66,31 @@ class Labeller():
         except AttributeError:
             pass
 
+        t = self.player.get_time()
+
         # toggle pause, skip forward/back
         if k == 'p':
             self.player.pause()
         elif k == 'l':
-            t = self.player.get_time()
             new_t = min(self.end_time - self.end_margin, t + self.skip_time)
             self.player.set_time(new_t)
         elif k == 'k':
-            t = self.player.get_time()
             new_t = max(0, t - self.skip_time)
             self.player.set_time(new_t)
+        # move to next video
+        elif k == 'n':
+            # TODO: maybe add some checks to make sure all the labels were set b4 moving on
+            self.running = False
 
         # set labels
         elif k == 'q':
-            self.intro_start = self.player.get_time()
+            self.intro_start = t
         elif k == 'w':
-            self.intro_end = self.player.get_time()
+            self.intro_end = t
         elif k == 'e':
-            self.outro_start = self.player.get_time()
+            self.outro_start = t
         elif k == 'r':
-            self.outro_end = self.player.get_time()
+            self.outro_end = t
 
         # jump to labelled times
         elif k == 'a':
@@ -100,4 +113,15 @@ class Labeller():
             if self.player.get_state != vlc.State.Playing:
                 self.player.play()
 
-Labeller("/home/ezhang/anime/video_folder/anime.mp4")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("video_folder")
+    #parser.add_argument("output_folder")
+    args = parser.parse_args()
+
+    video_folder = args.video_folder
+    video_files = sorted(glob.glob(os.path.join(video_folder, "*.mp4")))
+
+    labeller = Labeller()
+    for f in video_files:
+        labeller.run(f)
