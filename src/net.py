034b8661d1b7fb49ee774a47a744d1ae92b4db2b
Eric Zhang <ezhang887@gmail.com>
2020-08-07 20:09:35 -0700

    Format with black

--- src/net.py
+++ src/net.py
@@ -4,6 +4,7 @@ import math
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Net(nn.Module):
     def __init__(self, max_length, batch_size):
         super(Net, self).__init__()
@@ -11,10 +12,19 @@ class Net(nn.Module):
         conv1_stride = 10
         conv1_out_channels = 4
         self.conv1_pool_size = 5
-        self.conv1 = nn.Conv1d(2, conv1_out_channels, conv1_kernel_size, stride=conv1_stride)
-        self.fc1_input_size = self.__conv_pool_output_dim(max_length, conv1_out_channels, conv1_kernel_size, conv1_stride, self.conv1_pool_size, batch_size)
+        self.conv1 = nn.Conv1d(
+            2, conv1_out_channels, conv1_kernel_size, stride=conv1_stride
+        )
+        self.fc1_input_size = self.__conv_pool_output_dim(
+            max_length,
+            conv1_out_channels,
+            conv1_kernel_size,
+            conv1_stride,
+            self.conv1_pool_size,
+            batch_size,
+        )
         self.fc1 = nn.Linear(self.fc1_input_size, 4)
-    
+
     def forward(self, x):
         # Input (x) shape: [batch_size, input_channels (2), max_length]
         x = F.relu(self.conv1(x))
@@ -23,7 +33,19 @@ class Net(nn.Module):
         x = self.fc1(x)
         return x
 
-    def __conv_pool_output_dim(self, input_size, num_channels, conv_kernel_size, conv_stride, pool_kernel_size, batch_size):
-        conv_output_dim = math.floor((input_size - (conv_kernel_size - 1) - 1) / conv_stride + 1)
-        pool_output_dim = math.floor((conv_output_dim - (pool_kernel_size - 1) - 1) / pool_kernel_size + 1)
+    def __conv_pool_output_dim(
+        self,
+        input_size,
+        num_channels,
+        conv_kernel_size,
+        conv_stride,
+        pool_kernel_size,
+        batch_size,
+    ):
+        conv_output_dim = math.floor(
+            (input_size - (conv_kernel_size - 1) - 1) / conv_stride + 1
+        )
+        pool_output_dim = math.floor(
+            (conv_output_dim - (pool_kernel_size - 1) - 1) / pool_kernel_size + 1
+        )
         return pool_output_dim * num_channels * batch_size
