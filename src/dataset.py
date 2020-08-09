dfeed3af7d58eb3883cf898ddd00c7e54b9f2f54
Nicholas Liu <nicholas.j.liu@gmail.com>
2020-08-09 01:01:45 -0700

    Add parameter to dataset to load all data into memory at init

--- src/dataset.py
+++ src/dataset.py
@@ -14,15 +14,24 @@ class AnimeAudioDataset(Dataset):
     
     Labels are all loaded into memory and normalized during initialization
     """
-
-    def __init__(self, device):
-
+   
+    def __init__(self, device, audio_dir, label_dir, load_all_in_mem=False):
+        
+        self.load_all = load_all_in_mem
         self.device = device
 
-        self.audio_dir = "data/Audio"
-        self.label_dir = "data/Labels"  # change this to whatever
+        self.audio_dir = audio_dir
+        self.label_dir = label_dir
 
-        self.audio_filenames, self.max_length = self._load_audio()
+        if self.load_all:
+            self.audio_filenames, self.max_length, self.audio_data = self._load_audio()
+            for i in range(len(self.audio_data)):
+                self.audio_data[i] = self._pad_audio(self.audio_data[i])
+            self.audio_data = torch.cat(self.audio_data)
+            self.audio_data.to(self.device)
+        else:
+            self.audio_filenames, self.max_length, _ = self._load_audio()
+        
         raw_labels = self._load_labels(self.audio_filenames)
         self.labels, self.l_mean, self.l_std = self._normalize_labels(raw_labels)
         self.labels = self.labels.to(self.device)
@@ -43,18 +52,20 @@ class AnimeAudioDataset(Dataset):
         [str, ...], int
             array of filenames, maximum length of all audio segments
         """
+        audio_data = [] # Used only if we can load all the data into memory
         audio_filenames = []
         max_length = 0
 
         for a in os.listdir(self.audio_dir):
             audio_filenames.append(a)
-
             audio_filename = os.path.join(self.audio_dir, a)
             waveform, _ = torchaudio.load(audio_filename)
+
+            if self.load_all:
+                audio_data.append(waveform)
             max_length = max(max_length, waveform.shape[1])
-
-        return audio_filenames, max_length
-
+        return audio_filenames, max_length, audio_data
+    
     def _load_labels(self, audio_filenames):
         """loads the labels corresponding to the audio filenames
 
@@ -120,10 +131,14 @@ class AnimeAudioDataset(Dataset):
         return len(self.audio_filenames)
 
     def __getitem__(self, idx):
-        # idx can be a tensor
-        audio_filename = os.path.join(self.audio_dir, self.audio_filenames[idx])
-        waveform, _ = torchaudio.load(audio_filename)
-        # padded_audio = self._pad_audio([waveform])
-        padded_audio = self._pad_audio(waveform)
-        padded_audio = padded_audio.to(self.device)
-        return padded_audio, self.labels[idx]
+       # idx can be a tensor'
+        if self.load_all:
+            print("loading from gpu mem")
+            return self.audio_data[idx], self.labels[idx]
+        else:
+            audio_filename = os.path.join(self.audio_dir, self.audio_filenames[idx])
+            waveform, _ = torchaudio.load(audio_filename)
+            
+            padded_audio = self._pad_audio(waveform)
+            padded_audio = padded_audio.to(self.device)
+            return padded_audio, self.labels[idx]
