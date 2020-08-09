fb3ad15de24c5508a8f6990e00be097b210724e4
Eric Zhang <ezhang887@gmail.com>
2020-08-09 00:58:40 -0700

    Remove "magic number" (#7)
    * Calculate "magic number"
    
    * Format with black
    
    * Add some docs

--- src/dataset.py
+++ src/dataset.py
@@ -6,6 +6,7 @@ import os
 
 from torch.utils.data import Dataset
 
+
 class AnimeAudioDataset(Dataset):
     """
     Load in audio file names and get the length of the largest audio sample.
@@ -13,28 +14,27 @@ class AnimeAudioDataset(Dataset):
     
     Labels are all loaded into memory and normalized during initialization
     """
-    
+
     def __init__(self, device):
-        
+
         self.device = device
 
-        self.audio_dir = 'data/Audio'
-        self.label_dir = 'data/Labels' # change this to whatever
-        
+        self.audio_dir = "data/Audio"
+        self.label_dir = "data/Labels"  # change this to whatever
+
         self.audio_filenames, self.max_length = self._load_audio()
         raw_labels = self._load_labels(self.audio_filenames)
-        print(raw_labels)
         self.labels, self.l_mean, self.l_std = self._normalize_labels(raw_labels)
         self.labels = self.labels.to(self.device)
-    
+
         assert len(self.audio_filenames) == len(self.labels)
-        
-        '''
+
+        """
         self.data = self._pad_audio(self._load_audio())
         self.labels, self.label_mean, self.label_std = \
                 self._normalize_labels(self._load_labels())
-        '''
-        
+        """
+
     def _load_audio(self):
         """loads in audio filenames and finds maximum length of all audio segments
 
@@ -45,16 +45,16 @@ class AnimeAudioDataset(Dataset):
         """
         audio_filenames = []
         max_length = 0
-        
+
         for a in os.listdir(self.audio_dir):
             audio_filenames.append(a)
-            
+
             audio_filename = os.path.join(self.audio_dir, a)
             waveform, _ = torchaudio.load(audio_filename)
             max_length = max(max_length, waveform.shape[1])
-        
+
         return audio_filenames, max_length
-    
+
     def _load_labels(self, audio_filenames):
         """loads the labels corresponding to the audio filenames
 
@@ -76,26 +76,26 @@ class AnimeAudioDataset(Dataset):
             but attempts to finish and raises error after
         """
         labels = []
-        error  = False
-        
+        error = False
+
         for filename in audio_filenames:
-            file = os.path.splitext(filename)[0] + '.label'
+            file = os.path.splitext(filename)[0] + ".label"
             filepath = os.path.join(self.label_dir, file)
             label = [0] * 4
             try:
-                f = open(filepath, 'r')
+                f = open(filepath, "r")
                 for i in range(4):
                     label[i] = int(f.readline())
                 labels.append(label)
             except BaseException as e:
                 print(e)
                 error = True
-        
+
         if error:
-            raise RuntimeError('Failed to load labels')
-            
+            raise RuntimeError("Failed to load labels")
+
         return torch.Tensor(labels)
-                
+
     def _normalize_labels(self, labels):
         """normalizes labels off entire mean and std
         returns labels, mean, std
@@ -104,21 +104,21 @@ class AnimeAudioDataset(Dataset):
         l_std = labels.std(dim=0)
         labels = (labels - l_mean) / l_std
         return labels, l_mean, l_std
-        
+
     def _pad_audio(self, data):
         # longest = max(map(lambda x: x.shape[1], data))
-        '''
+        """
         for i in range(len(data)):
             zeros = torch.zeros(2, (self.max_length - data[i].shape[1]))
             data[i] = torch.cat((data[i], zeros), dim=1)
         return torch.stack(data)
-        '''
+        """
         zeros = torch.zeros(2, (self.max_length - data.shape[1]))
         return torch.cat((data, zeros), dim=1)
-    
+
     def __len__(self):
         return len(self.audio_filenames)
-    
+
     def __getitem__(self, idx):
         # idx can be a tensor
         audio_filename = os.path.join(self.audio_dir, self.audio_filenames[idx])
--- src/net.py
+++ src/net.py
@@ -1,25 +1,72 @@
 #!/usr/bin/env python3
 
+import math
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, max_length, batch_size):
         super(Net, self).__init__()
-        self.magic_num = 1103908 #234395 * 4
-        self.conv1 = nn.Conv1d(2, 4, 1600, stride=10)
-        self.pool = nn.MaxPool1d(5)
-        # self.conv2 = nn.Conv1d(4, 8, 400, stride=10)
-        self.fc1 = nn.Linear(self.magic_num, 4)
-        # self.fc2 = nn.Linear(10000, 100)
-        # self.fc3 = nn.Linear(100, 4)
-    
+        conv1_kernel_size = 1600
+        conv1_stride = 10
+        conv1_out_channels = 4
+        self.conv1_pool_size = 5
+        self.conv1 = nn.Conv1d(
+            2, conv1_out_channels, conv1_kernel_size, stride=conv1_stride
+        )
+        self.fc1_input_size = self.__conv_pool_output_dim(
+            max_length,
+            conv1_out_channels,
+            conv1_kernel_size,
+            conv1_stride,
+            self.conv1_pool_size,
+            batch_size,
+        )
+        self.fc1 = nn.Linear(self.fc1_input_size, 4)
+
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        # x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, self.magic_num)
-        # x = F.relu(self.fc1(x))
-        # x = F.relu(self.fc2(x))
-        # x = self.fc3(x)
+        # Input (x) shape: [batch_size, input_channels (2), max_length]
+        x = F.relu(self.conv1(x))
+        x = F.max_pool1d(x, self.conv1_pool_size)
+        x = x.view(-1, self.fc1_input_size)
         x = self.fc1(x)
         return x
+
+    def __conv_pool_output_dim(
+        self,
+        input_size,
+        num_channels,
+        conv_kernel_size,
+        conv_stride,
+        pool_kernel_size,
+        batch_size,
+    ):
+        """
+        Calculate the size of the flatted conv/pool layer going into the fully connected layer.
+        See https://pytorch.org/docs/stable/generated/torch.nn.Conv1d.html and
+            https://pytorch.org/docs/master/generated/torch.nn.MaxPool1d.html for the formulas.
+
+        Parameters
+        ----------
+        input_size: int
+            number of datapoints in the input of the final conv layer
+        num_channels: int
+            number of channels of the data
+        conv_kernel_size: int
+            size of the kernel for the final conv layer
+        conv_stride: int
+            stride for the final conv layer
+        pool_kernel_size: int
+            size of the kernel for the max pool layer after the conv layer
+        batch_size: int
+            batch_size we are training with
+
+        """
+        conv_output_dim = math.floor(
+            (input_size - (conv_kernel_size - 1) - 1) / conv_stride + 1
+        )
+        pool_output_dim = math.floor(
+            (conv_output_dim - (pool_kernel_size - 1) - 1) / pool_kernel_size + 1
+        )
+        return pool_output_dim * num_channels * batch_size
